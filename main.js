1d32d2607a55945a9a1a95b8a9bcd9bdf5dbe68c
Ganesh Acharya <ganesh.a@juegostudio.com>
2019-09-24 17:09:06 +0530

    Initial files

---
+++ main.js
@@ -0,0 +1,13 @@
+navigator.geolocation.getCurrentPosition((success) => {
+    console.log(success)
+}, (failure) => {
+    console.log(failure)
+});
+
+function success() {
+    console.log("Success...")
+}
+
+function failure() {
+    console.log("Failed...")
+}
